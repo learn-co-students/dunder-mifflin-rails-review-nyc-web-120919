8ab14515cdfc236f51cdbe23bdb9b3619b8216d9
merewelch214 <merewelch@gmail.com>
2020-01-13 16:37:20 -0500

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,41 @@
 class EmployeesController < ApplicationController
+    def index
+        @employees = Employee.all 
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def new
+        @employee = Employee.new
+        @dogs = Dog.all
+    end
+
+    def create
+        @employee = Employee.create(employee_params)
+        if @employee.valid?
+            redirect_to employee_path(@employee)
+        else
+            flash[:message] = "Alias and title must be unique"
+            redirect_to new_employee_path
+        end
+    end
+
+    def edit
+        @dogs = Dog.all
+        @employee = Employee.find(params[:id])
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        @employee.update(employee_params)
+        @employee.save
+        redirect_to employee_path(@employee)
+    end
+
+    private
+    def employee_params
+        params.require(:employee).permit(:id, :first_name, :last_name, :alias, :title, :office, :image_url, :dog_id)
+    end
 end
