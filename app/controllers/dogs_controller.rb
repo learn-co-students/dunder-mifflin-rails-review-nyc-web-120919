372336cf7ab0089329638b8b86a8a02c2adf4b56
flatironschool <danliu277@gmail.com>
2020-01-14 13:01:23 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,17 @@
 class DogsController < ApplicationController
+    def show
+        @dog = Dog.find(params[:id])
+        @employees = @dog.employees
+    end
+
+    def index
+        sort = params[:sort]
+        if(!sort)
+            @dogs = Dog.all
+        else
+            dogs = Employee.group("dog_id").order("count_id desc").count("id")
+            @dogs = []
+            dogs.each{|key, value| @dogs.push(Dog.find(key))}
+        end
+    end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,10 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+
+    validates_uniqueness_of :alias, allow_blank: true, allow_nil: true
+    validates_uniqueness_of :title, allow_blank: true, allow_nil: true
+
+    def name
+        first_name + " " + last_name
+    end
 end
