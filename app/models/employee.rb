169071f9cd3667c6f0e8bb3e7c8fb939e9ad207d
Ben Kagan <kagan.benjamin@gmail.com>
2020-01-15 16:54:48 -0500

    Done.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,4 +3,12 @@ class Employee < ApplicationRecord
     def to_s
         self.first_name + " " + self.last_name
     end
+
+    def dog_names    
+        Dog.all.each do |dog|
+            if dog.id == @employee.dog_id 
+                dog.name 
+            end
+        end
+    end
 end
